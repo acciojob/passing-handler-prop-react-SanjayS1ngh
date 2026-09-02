f339b62b018d664aa9a9dfc78fe71dff829b3524
SanjayS1gh <samantsanjay100@gmail.com>
2026-09-03 04:45:55 +0530

    Complete passing handler prop assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
 
+const Selection = ({ applyColor }) => {
+    const [style, updateSelectionStyle] = useState({
+        background: ""
+    });
+
+    return (
+        <div
+            className="fix-box"
+            style={style}
+            onClick={() => applyColor(updateSelectionStyle)}
+        ></div>
+    );
+};
+
+export default Selection;
